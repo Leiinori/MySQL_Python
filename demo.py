1f25fca7d366a3016cd00df662a217a16d2c6fba
LeiMing <leiinori0821@gmail.com>
2025-06-24 15:04:34 +0800

    測試與練習用SQL語法查詢world資料庫中資訊

--- demo.py
+++ demo.py
@@ -6,11 +6,22 @@ cnx = mysql.connector.connect(
     host = "127.0.0.1",
     port = 3306,
     user = "dbuser",
-    password = "1234"
+    password = "1234",
+    database = "world"
 )
 
 print("透過連線取得cursor物件")
 
 dbcursor = cnx.cursor()
-print("執行 select name from city")
+
+# 執行 select name from city
 dbcursor.execute("select name from world.city")
+for cityname in dbcursor:
+    print(cityname)
+
+print()
+
+# 執行 select name, population from country
+dbcursor.execute("select name, population from country")
+for (c, p) in dbcursor:
+    print(c, p)
